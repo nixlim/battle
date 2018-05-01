438eed0e08f113025b7a82e849465dd9df04945f
Igor Ryabchuk <riabtchuk@gmail.com>
2018-05-01 14:16:52 +0100

    implementing post redirect loop

--- app.rb
+++ app.rb
@@ -6,10 +6,15 @@ class Battle < Sinatra::Base
   get '/' do
     erb(:index)
   end
-  post '/names' do
-    @name_p1 = params[:Player_1_Name]
-    @name_p2 = params[:Player_2_Name]
-    p params
+
+  get '/play' do
+    @name_p1 = session[:name1]
+    @name_p2 = session[:name2]
     erb(:play)
+  end
+  post '/names' do
+    session[:name1] = params[:Player_1_Name]
+    session[:name2] = params[:Player_2_Name]
+    redirect '/play'
   end
 end
